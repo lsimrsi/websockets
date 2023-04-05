fda5aa94eb3313c346d4d7a98e441414778da07e
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-05 13:46:20 -0500

    add input

---
+++ ui/src/lib/input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let label: string;
+  export let id: string;
+  let value = "";
+</script>
+
+<div>
+  <label for={id}>{label}</label>
+  <input {id} bind:value />
+</div>
