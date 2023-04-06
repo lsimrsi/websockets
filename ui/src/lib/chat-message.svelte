a6cead4e2be18cea8160ce9494e9da4bf77fa8e8
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-06 15:25:38 -0500

    style name

--- ui/src/lib/chat-message.svelte
+++ ui/src/lib/chat-message.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import type { ChatMessage } from "../interfaces";
+  import { name } from "../stores";
   export let chatMsg: ChatMessage;
+
+  let nameStyle = chatMsg.name === $name ? "text-blue-500" : "text-zinc-500";
 </script>
 
 <div class="flex flex-col mb-4">
-  <span class="font-bold text-zinc-600 min-w-[8rem]">
+  <span class={`font-bold ${nameStyle} min-w-[8rem]`}>
     {chatMsg.name}
   </span>
   <span class="border-l-2 pl-4 text-zinc-500">
