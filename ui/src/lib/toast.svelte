28e8b636ad7de84ce00f1127d54c3d468c0074a9
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-07 13:31:43 -0500

    fix styling

--- ui/src/lib/toast.svelte
+++ ui/src/lib/toast.svelte
@@ -1,31 +1,7 @@
 <script lang="ts">
   import { ToastType, type ToastItem } from "../interfaces";
 
-  import { quintOut } from "svelte/easing";
-  import { crossfade } from "svelte/transition";
-  import { flip } from "svelte/animate";
-
-  const [send, receive] = crossfade({
-    fallback(node, params) {
-      const style = getComputedStyle(node);
-      const transform = style.transform === "none" ? "" : style.transform;
-
-      return {
-        duration: 600,
-        easing: quintOut,
-        css: (t) => `
-					transform: ${transform} scale(${t});
-					opacity: ${t}
-				`,
-      };
-    },
-  });
-
   export let toast: ToastItem;
-
-  // let anim = "300ms ease-out 0s 1 normal none running ";
-  // const animation = toast.show ? anim + "fadeIn" : anim + "fadeOut";
-  // const top = props.index * 100;
 
   let bg = "";
   switch (toast.type) {
@@ -46,19 +22,11 @@
   }
 </script>
 
-<div
-  in:receive={{ key: toast.uuid }}
-  out:send={{ key: toast.uuid }}
-  class={`flex right-0 w-96 transition-[top] rounded-bl-lg rounded-tr-lg mb-2 border shadow bg-white`}
->
-  <div class={`flex rounded-bl-lg ${bg}`}>
-    <span
-      class={`m-auto text-center text-[1.5rem] text-stone-800 font-bold p-2`}
-    >
-      {toast.type}
-    </span>
-  </div>
-  <div class="flex text-[1.2rem] text-stone-800 p-2">
-    <span class="m-auto">{toast.message}</span>
-  </div>
+<div class={`flex rounded-bl-lg ${bg}`}>
+  <span class={`m-auto text-center text-[1.5rem] text-stone-800 font-bold p-2`}>
+    {toast.type}
+  </span>
+</div>
+<div class="flex text-[1.2rem] text-stone-800 p-2">
+  <span class="m-auto">{toast.message}</span>
 </div>
--- ui/src/lib/toasts.svelte
+++ ui/src/lib/toasts.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
   import { toasts } from "../stores";
   import Toast from "./toast.svelte";
+
+  import { quintOut } from "svelte/easing";
+  import { crossfade } from "svelte/transition";
+  import { flip } from "svelte/animate";
+
+  const [send, receive] = crossfade({
+    fallback(node, params) {
+      const style = getComputedStyle(node);
+      const transform = style.transform === "none" ? "" : style.transform;
+
+      return {
+        duration: 600,
+        easing: quintOut,
+        css: (t) => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`,
+      };
+    },
+  });
 </script>
 
 <div data-testid="toasts" class="absolute top-4 right-4 pointer-events-none">
   {#each $toasts as toast (toast.uuid)}
-    <Toast {toast} />
+    <div
+      in:receive={{ key: toast.uuid }}
+      out:send={{ key: toast.uuid }}
+      animate:flip
+      class={`flex right-0 w-96 transition-[top] rounded-bl-lg rounded-tr-lg mb-2 border shadow bg-white`}
+    >
+      <Toast {toast} />
+    </div>
   {/each}
 </div>
