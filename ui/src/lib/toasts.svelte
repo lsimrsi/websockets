2a1d44fd4eea617d046ee3026cfcf341ef61b757
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-07 14:23:14 -0500

    add easing

--- ui/src/lib/toasts.svelte
+++ ui/src/lib/toasts.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import { toasts } from "../stores";
   import Toast from "./toast.svelte";
-  // import { quintOut } from "svelte/easing";
   import { fly } from "svelte/transition";
   import { flip } from "svelte/animate";
+  import { cubicInOut, cubicOut } from "svelte/easing";
 </script>
 
 <div data-testid="toasts" class="absolute top-4 right-4 pointer-events-none">
   {#each $toasts as toast (toast.uuid)}
     <div
-      in:fly={{ y: 50, duration: 200 }}
-      out:fly={{ y: -50, duration: 600 }}
+      in:fly={{ y: 50, duration: 200, easing: cubicOut }}
+      out:fly={{ y: -50, duration: 600, easing: cubicInOut }}
       animate:flip={{ duration: 200 }}
       class={`flex right-0 w-96 transition-[top] rounded-bl-lg rounded-tr-lg mb-2 border shadow bg-white`}
     >
