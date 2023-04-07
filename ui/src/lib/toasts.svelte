64e939ee92b847b83c8d1bc0adffc084da71324f
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-07 14:03:19 -0500

    c

--- ui/src/lib/toasts.svelte
+++ ui/src/lib/toasts.svelte
@@ -1,29 +1,9 @@
 <script lang="ts">
   import { toasts } from "../stores";
   import Toast from "./toast.svelte";
-
-  import { quintOut } from "svelte/easing";
+  // import { quintOut } from "svelte/easing";
   import { fly } from "svelte/transition";
   import { flip } from "svelte/animate";
-
-  // const [send, receive] = crossfade({
-  //   fallback(node, params) {
-  //     const style = getComputedStyle(node);
-  //     const transform = style.transform === "none" ? "" : style.transform;
-
-  //     return {
-  //       duration: 400,
-  //       easing: quintOut,
-  //       css: (t) => {
-  //         let y = (1 - t) * 50;
-  //         return `
-  // 				transform: ${transform} translateY(${y}px);
-  // 				opacity: ${t}
-  // 			`;
-  //       },
-  //     };
-  //   },
-  // });
 </script>
 
 <div data-testid="toasts" class="absolute top-4 right-4 pointer-events-none">
