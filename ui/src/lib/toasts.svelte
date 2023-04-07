cfeb8af157836f28c91a0f058e7e15b334f6cc33
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-07 13:48:59 -0500

    nicer animation

--- ui/src/lib/toasts.svelte
+++ ui/src/lib/toasts.svelte
@@ -3,31 +3,34 @@
   import Toast from "./toast.svelte";
 
   import { quintOut } from "svelte/easing";
-  import { crossfade } from "svelte/transition";
+  import { fly } from "svelte/transition";
   import { flip } from "svelte/animate";
 
-  const [send, receive] = crossfade({
-    fallback(node, params) {
-      const style = getComputedStyle(node);
-      const transform = style.transform === "none" ? "" : style.transform;
+  // const [send, receive] = crossfade({
+  //   fallback(node, params) {
+  //     const style = getComputedStyle(node);
+  //     const transform = style.transform === "none" ? "" : style.transform;
 
-      return {
-        duration: 200,
-        easing: quintOut,
-        css: (t) => `
-					transform: ${transform} scale(${t});
-					opacity: ${t}
-				`,
-      };
-    },
-  });
+  //     return {
+  //       duration: 400,
+  //       easing: quintOut,
+  //       css: (t) => {
+  //         let y = (1 - t) * 50;
+  //         return `
+  // 				transform: ${transform} translateY(${y}px);
+  // 				opacity: ${t}
+  // 			`;
+  //       },
+  //     };
+  //   },
+  // });
 </script>
 
 <div data-testid="toasts" class="absolute top-4 right-4 pointer-events-none">
   {#each $toasts as toast (toast.uuid)}
     <div
-      in:receive={{ key: toast.uuid }}
-      out:send={{ key: toast.uuid }}
+      in:fly={{ y: 50, duration: 200 }}
+      out:fly={{ y: -50, duration: 600 }}
       animate:flip={{ duration: 200 }}
       class={`flex right-0 w-96 transition-[top] rounded-bl-lg rounded-tr-lg mb-2 border shadow bg-white`}
     >
