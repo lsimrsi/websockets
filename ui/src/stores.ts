4be9a7efa407fec4d0758e681967e955031f167d
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-07 14:26:49 -0500

    c

--- ui/src/stores.ts
+++ ui/src/stores.ts
@@ -1,7 +1,5 @@
 import { writable, type Writable } from "svelte/store";
-import type { ChatMessage, Toast, ToastItem } from "./interfaces";
-import { v4 as uuidv4 } from "uuid";
-
+import type { ChatMessage, ToastItem } from "./interfaces";
 export const socket: Writable<WebSocket | null> = writable(null);
 export const messages: Writable<ChatMessage[]> = writable([]);
 export const name: Writable<string> = writable("");
