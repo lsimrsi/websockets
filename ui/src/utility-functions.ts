2eb586a1852ea404330affa933a49d76068d2f7d
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-07 12:21:58 -0500

    add utility fn

--- ui/src/utility-functions.ts
+++ ui/src/utility-functions.ts
@@ -1,6 +1,8 @@
-import type { ServerMessage } from './interfaces';
-import { socket } from './stores';
+import { TOAST_DURATION } from './constants';
+import type { ServerMessage, Toast, ToastItem } from './interfaces';
+import { socket, toasts } from './stores';
 import { get } from 'svelte/store';
+import { v4 as uuidv4 } from "uuid";
 
 export function sendMessage(msg: ServerMessage) {
   let $socket = get(socket);
@@ -10,4 +12,26 @@ export function sendMessage(msg: ServerMessage) {
   }
   console.log('sending msg', msg);
   $socket.send(JSON.stringify(msg));
+}
+
+/** Add a new toast to the queue. */
+function addNewToast(toast: Toast) {
+  let $toasts = get(toasts);
+  let uuid = uuidv4();
+  let item = {
+    type: toast.type,
+    message: toast.message,
+    uuid,
+    show: true,
+  };
+
+  $toasts = [...$toasts, item];
+
+  setTimeout(() => {
+    let $toastsLater = get(toasts);
+    let filteredToasts = $toastsLater.filter(
+      (ti: ToastItem) => ti.uuid !== item.uuid
+    );
+    $toastsLater = [...filteredToasts];
+  }, TOAST_DURATION);
 }
