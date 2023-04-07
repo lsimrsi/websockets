fa6328882599030aa9d8710457a0b1261d404619
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-07 12:20:45 -0500

    add toast types

---
+++ ui/src/constants.ts
@@ -0,0 +1,2 @@
+/** Toast duration in milliseconds. */
+export const TOAST_DURATION: number = 4000;
--- ui/src/interfaces.ts
+++ ui/src/interfaces.ts
@@ -7,3 +7,24 @@ export interface ChatMessage {
   name: string;
   message: string;
 }
+
+/** Temporary message that is displayed to the user. */
+export interface Toast {
+  type: ToastType;
+  message: string;
+}
+
+/** Manages state for the toast queue. */
+export interface ToastItem extends Toast {
+  uuid: string;
+  show: boolean;
+}
+
+export enum ToastType {
+  Network = "Network",
+  Info = "Info",
+  Success = "Success",
+  Redirect = "Redirect",
+  Client = "Client Error",
+  Server = "Server Error",
+}
