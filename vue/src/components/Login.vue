9c13a15a478d2417c0ba181da4d603dfa90c2f62
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-12 10:59:50 -0500

    add chat message, login components

---
+++ vue/src/components/Login.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { useNameStore, useSocketStore } from "../stores";
+  // import Input from "./input.svelte";
+
+  const name = useNameStore();
+  const socket = useSocketStore();
+
+  function onSubmit(e: Event) {
+    e.preventDefault();
+    console.log("name", name.name);
+    socket.sendMessage({ msg_type: "RegisterName", data: name.name });
+  }
+</script>
+
+<template>
+  <form class="m-auto" :submit={onSubmit}>
+    <label>Please enter your name</label>
+    <input v-model="name.name"/>
+  </form>
+</template>
