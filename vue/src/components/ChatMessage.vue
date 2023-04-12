5887f399dd0148fadab39eec48bedd77a38e8288
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-04-12 15:42:13 -0500

    fix name color

--- vue/src/components/ChatMessage.vue
+++ vue/src/components/ChatMessage.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
   import type { ChatMessage } from "../interfaces";
-  // import { useNameStore } from "../stores";
+  import { useNameStore } from "../stores";
 
-  defineProps<{
+  const p = defineProps<{
     chatMsg: ChatMessage
   }>()
 
-  // const name = useNameStore();
+  const name = useNameStore();
 
-  // let nameStyle = chatMsg.name === $name ? "text-blue-500" : "text-zinc-500";
+  let nameStyle = p.chatMsg.name === name.name ? "text-blue-500" : "text-zinc-500";
 </script>
 
 <template>
   <div class="flex flex-col mb-4">
-    <span class="`font-bold min-w-[8rem]`">
+    <span :class="`font-bold ${nameStyle} min-w-[8rem]`">
       {{chatMsg.name}}
     </span>
     <span class="border-l-2 pl-4 text-zinc-500">
